f78f7a237bd5b648a9ffcbd48eb5c70dddc9d26e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-16 18:21:42 -0300

    Agrego id a Genero

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/dominio/Genero.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/dominio/Genero.java
@@ -3,13 +3,25 @@ package com.besysoft.ejercitacion.dominio;
 import java.util.ArrayList;
 
 public class Genero {
+    private int id;
     private String nombre;
     private ArrayList <Pelicula> listaPelis;
 
-    public Genero(String nombre) {
-        this.nombre = nombre;
-    }
     public Genero() {
+    }
+
+    public Genero(int id, String nombre, ArrayList<Pelicula> listaPelis) {
+        this.id = id;
+        this.nombre = nombre;
+        this.listaPelis = listaPelis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNombre() {
