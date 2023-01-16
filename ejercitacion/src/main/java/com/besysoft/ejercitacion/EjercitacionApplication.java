7916e4bcfced3c771d4694dfc6cd376aab4b373f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-16 13:32:22 -0300

    Crea estructura de proyecto para ejercitacion

---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/EjercitacionApplication.java
@@ -0,0 +1,13 @@
+package com.besysoft.ejercitacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercitacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercitacionApplication.class, args);
+	}
+
+}
---
+++ ejercitacion/src/test/java/com/besysoft/ejercitacion/EjercitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besysoft.ejercitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
