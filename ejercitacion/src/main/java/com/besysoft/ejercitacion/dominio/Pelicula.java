a7c6a961e099d4a2aa7cc23c5dabfd2dae23f917
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-16 17:52:50 -0300

    Modifica tipado de fecha en Pelicula

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/dominio/Pelicula.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/dominio/Pelicula.java
@@ -1,21 +1,32 @@
 package com.besysoft.ejercitacion.dominio;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Pelicula {
+    private int id;
     private String titulo;
-    private Date fechaCreacion;
+    private LocalDate fechaCreacion;
     private int calificacion;
     private ArrayList<Personaje> listaPersonajes;
 
     public Pelicula() {
     }
 
-    public Pelicula(String titulo, Date fechaCreacion, int calificacion) {
+    public Pelicula(int id, String titulo, LocalDate fechaCreacion, int calificacion, ArrayList<Personaje> listaPersonajes) {
+        this.id = id;
         this.titulo = titulo;
         this.fechaCreacion = fechaCreacion;
         this.calificacion = calificacion;
+        this.listaPersonajes = listaPersonajes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitulo() {
@@ -26,11 +37,11 @@ public class Pelicula {
         this.titulo = titulo;
     }
 
-    public Date getFechaCreacion() {
+    public LocalDate getFechaCreacion() {
         return fechaCreacion;
     }
 
-    public void setFechaCreacion(Date fechaCreacion) {
+    public void setFechaCreacion(LocalDate fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
     }
 
