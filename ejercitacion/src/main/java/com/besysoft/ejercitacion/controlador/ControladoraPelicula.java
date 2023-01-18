a074f73f2eb16dd0d0267a7ccb8043756acddf49
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-18 16:13:55 -0300

    Elimina dependencias no utilizadas

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
@@ -3,14 +3,12 @@ package com.besysoft.ejercitacion.controlador;
 import com.besysoft.ejercitacion.Test;
 import com.besysoft.ejercitacion.dominio.Genero;
 import com.besysoft.ejercitacion.dominio.Pelicula;
-import com.besysoft.ejercitacion.dominio.Personaje;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
