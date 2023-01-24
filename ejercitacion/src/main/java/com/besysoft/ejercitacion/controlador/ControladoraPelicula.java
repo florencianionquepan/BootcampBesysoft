d924100c5fcf029353e1dcb4ab64926edae7dab4
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-24 15:53:22 -0300

    Utiliza RequestMapping fuera de los controladores

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
@@ -3,25 +3,24 @@ package com.besysoft.ejercitacion.controlador;
 import com.besysoft.ejercitacion.Test;
 import com.besysoft.ejercitacion.dominio.Genero;
 import com.besysoft.ejercitacion.dominio.Pelicula;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/peliculas")
 public class ControladoraPelicula {
 
-    @GetMapping("/peliculas")
+    @GetMapping
     public List<Pelicula> verPelis(){
         Test miTest=new Test();
         miTest.generarDatos();
         return miTest.getListaPelis();
     }
 
-    @GetMapping("/peliculas/{titulo}")
+    @GetMapping("/{titulo}")
     public List<Pelicula> buscarPeliByTitulo(@PathVariable String titulo){
         Test miTest=new Test();
         miTest.generarDatos();
@@ -31,7 +30,7 @@ public class ControladoraPelicula {
         return listaPelis;
     }
 
-    @GetMapping("/peliculas/genero/{genero}")
+    @GetMapping("/genero/{genero}")
     public List<Pelicula> buscarPeliByGenero(@PathVariable String genero){
         Test miTest=new Test();
         miTest.generarDatos();
--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPersonaje.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPersonaje.java
@@ -2,25 +2,24 @@ package com.besysoft.ejercitacion.controlador;
 
 import com.besysoft.ejercitacion.Test;
 import com.besysoft.ejercitacion.dominio.Personaje;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/personajes")
 public class ControladoraPersonaje {
 
-    @GetMapping("/personajes")
+    @GetMapping
     public List<Personaje> verPerso(){
         Test miTest=new Test();
         miTest.generarDatos();
         return miTest.getListaPerso();
     }
 
-    @GetMapping("/personajes/{nombre}")
+    @GetMapping("/{nombre}")
     public List<Personaje> buscarPersoByNombre(@PathVariable String nombre){
         Test miTest=new Test();
         miTest.generarDatos();
@@ -30,7 +29,7 @@ public class ControladoraPersonaje {
         return listaPerso;
     }
 
-    @GetMapping("/personajes/edad/{edad}")
+    @GetMapping("/edad/{edad}")
     public List<Personaje> buscarPersoByEdad(@PathVariable int edad){
         Test miTest=new Test();
         miTest.generarDatos();
