51a84ac4fbf8368251887cf6daef4a7dc044c590
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-24 16:36:43 -0300

    Agrega endpoint para buscar pelis por calificacion y fecha

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPelicula.java
@@ -3,8 +3,10 @@ package com.besysoft.ejercitacion.controlador;
 import com.besysoft.ejercitacion.Test;
 import com.besysoft.ejercitacion.dominio.Genero;
 import com.besysoft.ejercitacion.dominio.Pelicula;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,5 +43,29 @@ public class ControladoraPelicula {
                 .collect(Collectors.toList());
         ArrayList<Pelicula> listaPel=(listaPelis.size()>0)?listaPelis.get(0):new ArrayList<Pelicula>();
         return listaPel;
+    }
+
+    @GetMapping("/fechas")
+    public List<Pelicula> buscarPeliFechas(@RequestParam @DateTimeFormat(pattern = "ddMMyyyy") LocalDate desde ,
+                                            @RequestParam @DateTimeFormat(pattern = "ddMMyyyy") LocalDate hasta){
+        Test miTest=new Test();
+        miTest.generarDatos();
+        List<Pelicula> listaPelis=miTest.getListaPelis().stream()
+                .filter(pelicula -> pelicula.getFechaCreacion().isAfter(desde)
+                        && pelicula.getFechaCreacion().isBefore(hasta))
+                .collect(Collectors.toList());
+        return listaPelis;
+    }
+
+    @GetMapping("/calificacion")
+    public List<Pelicula> buscarPeliCalificacion(@RequestParam int desde,
+                                           @RequestParam int hasta){
+        Test miTest=new Test();
+        miTest.generarDatos();
+        List<Pelicula> listaPelis=miTest.getListaPelis().stream()
+                .filter(pelicula -> pelicula.getCalificacion()<hasta
+                        && pelicula.getCalificacion()>desde)
+                .collect(Collectors.toList());
+        return listaPelis;
     }
 }
