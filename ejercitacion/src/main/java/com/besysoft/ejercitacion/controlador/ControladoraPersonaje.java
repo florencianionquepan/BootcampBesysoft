b05f3cea06539348fb2f9f0256057e5c97462d2f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-24 21:21:38 -0300

    Crea altaPersonaje por metodo Post

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPersonaje.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraPersonaje.java
@@ -3,13 +3,11 @@ package com.besysoft.ejercitacion.controlador;
 import com.besysoft.ejercitacion.Test;
 import com.besysoft.ejercitacion.dominio.Pelicula;
 import com.besysoft.ejercitacion.dominio.Personaje;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,6 +26,13 @@ public class ControladoraPersonaje {
         mensajeBody.put("Success",Boolean.TRUE);
         mensajeBody.put("data",lista);
         return ResponseEntity.ok(mensajeBody);
+    }
+
+    private List<Personaje> buscarPersoByPeli(Pelicula peli){
+        List <Personaje> listaPerso=this.listaPerso.stream()
+                        .filter(perso->perso.getPelicula().getId()==peli.getId())
+                        .collect(Collectors.toList());
+        return listaPerso;
     }
     @GetMapping
     public ResponseEntity<?> verPerso(){
@@ -65,6 +70,24 @@ public class ControladoraPersonaje {
                 .collect(Collectors.toList());
         return this.successResponse(listaPerso);
 
+    }
+
+    @PostMapping
+    public ResponseEntity<?> altaPersonaje(@RequestBody Personaje perso){
+        perso.setId(this.listaPerso.size()+1);
+
+        this.listaPerso.add(perso);
+        this.setListaPerso(this.listaPerso);
+        //asociar este personaje a la peliculA. La pelicula no se entero de este personaje todavia:
+        //Aca primero dejar el optional por si el personaje se crea con una pelicula cuyo id no existe!!
+        //y enviar un error de bad request. Luego si existe ahi si hacer el get y setearle la nueva lista de personajes.Esto
+        //hacerlo primero entonces!!
+        Pelicula peliAsociada=ControladoraPelicula.getListaPelis().
+                stream().filter(peli->peli.getId()==perso.getPelicula().getId()).findAny().get();
+        //Asociarle los personajes de la pelicula nada massss
+        List<Personaje> listaPer=this.buscarPersoByPeli(peliAsociada);
+        peliAsociada.setListaPersonajes(listaPer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(perso);
     }
 
 
