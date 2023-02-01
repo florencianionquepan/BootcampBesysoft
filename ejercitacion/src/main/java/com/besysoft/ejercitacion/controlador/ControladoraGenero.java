dce0ca74b448009082f98daac95587fbe3297f68
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-01 14:34:12 -0300

    Control genero llama a service de genero y pelicula

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraGenero.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/ControladoraGenero.java
@@ -1,9 +1,8 @@
 package com.besysoft.ejercitacion.controlador;
 
-import com.besysoft.ejercitacion.dominio.Personaje;
-import com.besysoft.ejercitacion.utilidades.Test;
+import com.besysoft.ejercitacion.servicios.interfaces.IGeneroService;
 import com.besysoft.ejercitacion.dominio.Genero;
-import com.besysoft.ejercitacion.dominio.Pelicula;
+import com.besysoft.ejercitacion.servicios.interfaces.IPeliculaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,76 +12,62 @@ import java.util.*;
 @RestController
 @RequestMapping("/generos")
 public class ControladoraGenero {
+
+    private final IGeneroService genService;
+    private final IPeliculaService peliService;
     public Map<String,Object> mensajeBody= new HashMap<>();
-    public static List<Genero> listaGeneros= Test.getListaGeneros();
-    public List<Genero> getListaGeneros() {
-        return listaGeneros;
+
+    public ControladoraGenero(IGeneroService genService, IPeliculaService peliService) {
+        this.genService = genService;
+        this.peliService = peliService;
     }
-    public void setListaGeneros(List<Genero> listaGeneros) {
-        ControladoraGenero.listaGeneros = listaGeneros;
+
+    @GetMapping
+    public ResponseEntity<?>  verGeneros(){
+        mensajeBody.put("Success",Boolean.TRUE);
+        mensajeBody.put("data",this.genService.verGeneros());
+        return ResponseEntity.ok(mensajeBody);
     }
 
     @PostMapping
     public ResponseEntity<?> altaGenero(@RequestBody Genero genero){
-        this.porSiListaPelisNull(genero);
-        if(!ControladoraPelicula.sonPelisCorrectas(genero.getListaPelis())){
+        this.genService.porSiListaPelisNull(genero);
+        if(!this.peliService.sonPelisCorrectas(genero.getListaPelis())){
             mensajeBody.put("Success", Boolean.FALSE);
             mensajeBody.put("data", "Alguna pelicula ingresada no existe o no es correcta");
             return ResponseEntity
                     .badRequest()
                     .body(mensajeBody);
         }
-        genero.setId(getListaGeneros().size()+1);
-        this.getListaGeneros().add(genero);
-        this.setListaGeneros(listaGeneros);
-        return ResponseEntity.status(HttpStatus.CREATED).body(genero);
-    }
-    @GetMapping
-    public ResponseEntity<?>  verGeneros(){
-        mensajeBody.put("Success",Boolean.TRUE);
-        mensajeBody.put("data",listaGeneros);
-        return ResponseEntity.ok(mensajeBody);
+        Genero generoNuevo=this.genService.altaGenero(genero);
+        return ResponseEntity.status(HttpStatus.CREATED).body(generoNuevo);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> modiGenero(@RequestBody Genero genero,
                                          @PathVariable int id){
-        this.porSiListaPelisNull(genero);
-        Optional<Genero> oGenero=this.getListaGeneros()
-                                .stream()
-                                .filter(gen->gen.getId()==id)
-                                .findAny();
-        if(oGenero.isEmpty()) {
+        this.genService.porSiListaPelisNull(genero);
+
+        if(this.genService.existeGenero(id)) {
             mensajeBody.put("Success", Boolean.FALSE);
             mensajeBody.put("data", String.format("El genero con id %d ingresado no existe", id));
             return ResponseEntity
                     .badRequest()
                     .body(mensajeBody);
         }
-        if(!ControladoraPelicula.sonPelisCorrectas(genero.getListaPelis())){
+        if(!this.peliService.sonPelisCorrectas(genero.getListaPelis())){
             mensajeBody.put("Success", Boolean.FALSE);
             mensajeBody.put("data", "Alguna pelicula ingresada no existe");
             return ResponseEntity
                     .badRequest()
                     .body(mensajeBody);
         }
-        this.getListaGeneros().forEach(gen->{
-            if(gen.getId()==id) {
-                gen.setNombre(genero.getNombre());
-                gen.setListaPelis(genero.getListaPelis());
-            }
-        });
+            Genero generoMod=this.genService.modiGenero(genero,id);
             mensajeBody.put("Success",Boolean.TRUE);
-            mensajeBody.put("data",this.getListaGeneros().get(id-1));
+            mensajeBody.put("data",generoMod);
             return ResponseEntity.ok(mensajeBody);
     }
 
-    private void porSiListaPelisNull(Genero genero){
-        if(genero.getListaPelis()==null){
-            List<Pelicula> listaPelis=new ArrayList<>();
-            genero.setListaPelis(listaPelis);
-        }
-    }
 
 
 }
