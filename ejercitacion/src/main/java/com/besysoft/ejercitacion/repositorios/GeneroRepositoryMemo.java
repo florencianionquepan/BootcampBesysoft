6a14604f55d79693434aad3d16ecfc6f6883adaa
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-01 20:56:40 -0300

    Creo repository de genero

---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/repositorios/GeneroRepositoryMemo.java
@@ -0,0 +1,43 @@
+package com.besysoft.ejercitacion.repositorios;
+
+import com.besysoft.ejercitacion.dominio.Genero;
+import com.besysoft.ejercitacion.utilidades.Test;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class GeneroRepositoryMemo implements IGeneroRepository{
+    private List<Genero> listaGeneros= Test.listaGeneros;
+    @Override
+    public List<Genero> verGeneros() {
+        return this.listaGeneros;
+    }
+
+    @Override
+    public Genero altaGen(Genero gen) {
+        gen.setId(listaGeneros.size()+1);
+        this.listaGeneros.add(gen);
+        return gen;
+    }
+
+    @Override
+    public Genero modiGen(Genero gen,int id) {
+        Genero genero=gen;
+        genero.setId(id);
+        this.listaGeneros.forEach(g->{
+            if(g.getId()==gen.getId()){
+                g.setNombre(gen.getNombre());
+            }
+        });
+        return genero;
+    }
+
+    @Override
+    public Optional<Genero> buscarGeneroById(int id) {
+        Optional<Genero> oGen=this.listaGeneros.stream()
+                .filter(g->g.getId()==id).findAny();
+        return oGen;
+    }
+}
---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/repositorios/IGeneroRepository.java
@@ -0,0 +1,15 @@
+package com.besysoft.ejercitacion.repositorios;
+
+import com.besysoft.ejercitacion.dominio.Genero;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IGeneroRepository {
+    List<Genero> verGeneros();
+    Genero altaGen(Genero gen);
+    Genero modiGen(Genero gen,int id);
+    Optional<Genero> buscarGeneroById(int id);
+
+}
