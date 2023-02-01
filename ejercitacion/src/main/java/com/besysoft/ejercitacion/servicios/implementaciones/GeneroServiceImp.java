b034a0dfffff6bb41f84a3c8ddb69884d260a7fe
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-01 20:56:57 -0300

    Creo service de genero

---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/servicios/implementaciones/GeneroServiceImp.java
@@ -0,0 +1,52 @@
+package com.besysoft.ejercitacion.servicios.implementaciones;
+
+import com.besysoft.ejercitacion.dominio.Genero;
+import com.besysoft.ejercitacion.dominio.Pelicula;
+import com.besysoft.ejercitacion.repositorios.IGeneroRepository;
+import com.besysoft.ejercitacion.servicios.interfaces.IGeneroService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GeneroServiceImp implements IGeneroService {
+
+    private final IGeneroRepository genRepo;
+
+    public GeneroServiceImp(IGeneroRepository genRepo) {
+        this.genRepo = genRepo;
+    }
+
+    @Override
+    public List<Genero> verGeneros() {
+        return this.genRepo.verGeneros();
+    }
+
+    @Override
+    public Genero altaGenero(Genero genero) {
+        return this.genRepo.altaGen(genero);
+    }
+
+    @Override
+    public Genero modiGenero(Genero genero, int id) {
+        return this.genRepo.modiGen(genero,id);
+    }
+
+    public void porSiListaPelisNull(Genero genero){
+        if(genero.getListaPelis()==null){
+            List<Pelicula> listaPelis=new ArrayList<>();
+            genero.setListaPelis(listaPelis);
+        }
+    }
+
+    public boolean existeGenero(int id){
+        boolean existe=false;
+        Optional<Genero> gen=this.genRepo.buscarGeneroById(id);
+        if(gen.isPresent()){
+            existe=true;
+        }
+        return existe;
+    }
+}
---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/servicios/interfaces/IGeneroService.java
@@ -0,0 +1,16 @@
+package com.besysoft.ejercitacion.servicios.interfaces;
+
+import com.besysoft.ejercitacion.dominio.Genero;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface IGeneroService {
+    List<Genero> verGeneros();
+    Genero altaGenero(Genero genero);
+    Genero modiGenero(Genero genero, int id);
+    void porSiListaPelisNull(Genero genero);
+    boolean existeGenero(int id);
+
+}
