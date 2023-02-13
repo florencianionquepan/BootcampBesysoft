92cb9932c7a5e586a8749f862cca062f47e79e10
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-13 18:40:16 -0300

    Update GeneroMapper with PeliculaMapper

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/dto/mapper/GeneroMapper.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/dto/mapper/GeneroMapper.java
@@ -3,6 +3,7 @@ package com.besysoft.ejercitacion.dto.mapper;
 import com.besysoft.ejercitacion.dominio.Genero;
 import com.besysoft.ejercitacion.dto.GeneroReqDTO;
 import com.besysoft.ejercitacion.dto.GeneroRespDTO;
+import com.besysoft.ejercitacion.dto.PeliculaRespDTO;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,11 +12,18 @@ import java.util.stream.Collectors;
 @Component
 public class GeneroMapper implements IGeneroMapper{
 
+    private final IPeliculaMapper peliMap;
+
+    public GeneroMapper(IPeliculaMapper peliMap) {
+        this.peliMap = peliMap;
+    }
+
     public Genero mapToEntity(GeneroReqDTO dto){
         Genero genero=new Genero();
         genero.setId(dto.getId());
         genero.setNombre(dto.getNombre());
-        genero.setListaPelis(dto.getListaPelis());
+        List<PeliculaRespDTO> pelis=dto.getPeliculas();
+        genero.setListaPelis(this.peliMap.mapListToEntity(pelis));
         return genero;
     }
 
@@ -23,7 +31,7 @@ public class GeneroMapper implements IGeneroMapper{
         GeneroRespDTO dto=new GeneroRespDTO();
         dto.setId(entity.getId());
         dto.setNombre(entity.getNombre());
-        dto.setListaPelis(entity.getListaPelis());
+        dto.setPeliculas(this.peliMap.mapListToDto(entity.getListaPelis()));
         return dto;
     }
 
