68c1e6b8a98e4d01f64ad958b2476955429591c0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-01 02:56:32 -0300

    Create PersonajeServiceImpTest.java

---
+++ ejercitacion/src/test/java/com/besysoft/ejercitacion/servicios/implementaciones/PersonajeServiceImpTest.java
@@ -0,0 +1,202 @@
+package com.besysoft.ejercitacion.servicios.implementaciones;
+
+import com.besysoft.ejercitacion.dominio.Pelicula;
+import com.besysoft.ejercitacion.dominio.Personaje;
+import com.besysoft.ejercitacion.repositorios.database.PeliculaRepository;
+import com.besysoft.ejercitacion.repositorios.database.PersonajeRepository;
+import com.besysoft.ejercitacion.servicios.interfaces.IPeliculaService;
+import com.besysoft.ejercitacion.utilidades.TestDatos;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+class PersonajeServiceImpTest {
+
+    private PersonajeServiceImp persoService;
+    private PersonajeRepository persoRepo;
+    private IPeliculaService peliService;
+    private PeliculaRepository peliRepo;
+    private Logger logger= LoggerFactory.getLogger(PersonajeServiceImp.class);
+
+    @BeforeEach
+    void setUp() {
+        persoRepo=mock(PersonajeRepository.class);
+        peliService=mock(IPeliculaService.class);
+        peliRepo=mock(PeliculaRepository.class);
+        persoService=new PersonajeServiceImp(persoRepo,peliService,peliRepo);
+        TestDatos test=new TestDatos();
+        test.generarDatos();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void verPerso() {
+        //GIVEN
+        when(persoRepo.findAll())
+                .thenReturn(TestDatos.getListaPerso());
+        //WHEN
+        List<Personaje> listaPerso= persoService.verPerso();
+        //THEN
+        assertThat(listaPerso.size()).isEqualTo(6);
+        verify(persoRepo).findAll();
+    }
+
+    @Test
+    void buscarPersoByNombre() {
+        //GIVEN
+        when(persoRepo.findByName("Carl"))
+                .thenReturn(new ArrayList<Personaje>(
+                        Arrays.asList(TestDatos.getPersonajeCarl())
+                ));
+        //WHEN
+        List<Personaje> personajesCarl= persoService
+                .buscarPersoByNombre(TestDatos.getPersonajeCarl().getNombre());
+        //THEN
+        assertThat(personajesCarl.get(0)).isEqualTo(TestDatos.getPersonajeCarl());
+        verify(persoRepo).findByName("Carl");
+    }
+
+    @Test
+    void buscarPersoByEdad() {
+        //GIVEN
+        when(persoRepo.findByAge(11))
+                .thenReturn(new ArrayList<Personaje>(
+                        Arrays.asList(TestDatos.getPersonajeMiguel())
+                ));
+        //WHEN
+        List<Personaje> personajes= persoService
+                .buscarPersoByEdad(TestDatos.getPersonajeMiguel().getEdad());
+        //THEN
+        assertThat(personajes.size()).isEqualTo(1);
+        verify(persoRepo).findByAge(TestDatos.getPersonajeMiguel().getEdad());
+    }
+
+    @Test
+    void buscarPersoRangoEdad() {
+        //GIVEN
+        when(persoRepo.findBetweenAges(10,30))
+                .thenReturn(new ArrayList<Personaje>(
+                        Arrays.asList(TestDatos.getPersonajeMiguel())
+                ));
+        //WHEN
+        List<Personaje> personajes= persoService
+                .buscarPersoRangoEdad(10,30);
+        //THEN
+        assertThat(personajes.size()).isEqualTo(1);
+        verify(persoRepo).findBetweenAges(10,30);
+    }
+
+    @Test
+    void altaPersonaje() {
+        Personaje per=TestDatos.getPersonajeCarl();
+        //GIVEN
+        when(persoRepo.save(any()))
+                .thenReturn(per);
+        when(peliService.sonPelisCorrectas(per.getListaPeliculas()))
+                .thenReturn(true);
+        //WHEN
+        persoService.altaPersonaje(per);
+        //THEN
+        ArgumentCaptor<Personaje> persoArgumentCaptor= ArgumentCaptor.forClass(Personaje.class);
+        verify(persoRepo).save(persoArgumentCaptor.capture());
+
+        assertThat(persoArgumentCaptor.getValue()).isEqualTo(per);
+    }
+
+    @Test
+    void altaPersonajeError(){
+        Personaje per=TestDatos.getPersonajeCarl();
+        //given
+        when(peliService.sonPelisCorrectas(per.getListaPeliculas()))
+                .thenReturn(false);
+        //WHEN
+        //THEN
+        assertThat(persoService.altaPersonaje(per)).isEqualTo(null);
+    }
+
+    @Test
+    void modiPersonaje() {
+        Personaje per1=TestDatos.getPersonajeCarl();
+        //GIVEN
+        given(persoRepo.save(per1))
+                .willReturn(per1);
+        per1.setNombre("Carlos");
+        //WHEN
+        persoService.modiPersonaje(per1,per1.getId());
+        //THEN
+        assertThat(per1.getNombre()).isEqualTo("Carlos");
+    }
+
+    @Test
+    void modiPersonajeErrorUno() {
+        Personaje perso=TestDatos.getPersonajeCarl();
+        //GIVEN
+        when(peliService.sonPelisCorrectas(perso.getListaPeliculas()))
+                .thenReturn(false);
+        //WHEN
+        //THEN
+        assertThat(persoService.modiPersonaje(perso,perso.getId())).isEqualTo(null);
+    }
+
+    @Test
+    void addPersoPeliTest() {
+        //GIVEN
+        Personaje persoCarl = TestDatos.getPersonajeCarl();
+        Pelicula peliPrueba=TestDatos.getPeliculaUp();
+        List<Pelicula> listaPelis= new ArrayList<Pelicula>(
+                Arrays.asList(peliPrueba)
+        );
+        persoCarl.setListaPeliculas(listaPelis);
+        logger.info(" pelisPerso " + listaPelis);
+        //Pelicula peliGuardada = mock(Pelicula.class);
+        logger.info("peliPrueba "+ when(peliRepo.save(peliPrueba)).thenReturn(peliPrueba));
+        logger.info("a ver: " + when(peliRepo.findById(peliPrueba.getId()))
+                .thenReturn(Optional.of(peliPrueba)));
+        //WHEN
+        persoService.addPersoPelis(persoCarl);
+        //THEN
+        boolean resultado=peliPrueba.getListaPersonajes().contains(persoCarl);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void removePersoPelisTest(){
+        //given
+        Personaje miguel=TestDatos.getPersonajeMiguel();
+        miguel.setListaPeliculas(new ArrayList<Pelicula>(
+                Arrays.asList(TestDatos.getPeliculaCoco())
+        ));
+        logger.info("Miguel "+ miguel + miguel.getListaPeliculas());
+        Pelicula peliCoco=TestDatos.getPeliculaCoco();
+        peliCoco.setListaPersonajes(new ArrayList<>(
+                Arrays.asList(TestDatos.getPersonajeMiguel())
+        ));
+        logger.info("Peli coco"+ peliCoco + peliCoco.getListaPersonajes());
+        when(persoRepo.save(miguel)).thenReturn(miguel);
+        when(persoRepo.findById(miguel.getId())).thenReturn(Optional.of(miguel));
+        when(peliRepo.save(peliCoco)).thenReturn(peliCoco);
+        when(peliRepo.findById(peliCoco.getId())).thenReturn(Optional.of(peliCoco));
+        //when
+        persoService.removePersoPelis(miguel.getId());
+        //then
+        boolean result=peliCoco.getListaPersonajes().contains(miguel);
+        assertFalse(result);
+    }
+}
