1b48e662cd49f1333fe4fc1baf739b7c8fa6cd55
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-02 19:15:18 -0300

    Update ApiControllerAdvice.java

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/handlerException/ApiControllerAdvice.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/handlerException/ApiControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.besysoft.ejercitacion.controlador.handlerException;
 
 import com.besysoft.ejercitacion.dto.response.ExceptionDTO;
+import com.besysoft.ejercitacion.excepciones.ExistException;
+import com.besysoft.ejercitacion.excepciones.ListaIncorrectaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,4 +24,19 @@ public class ApiControllerAdvice {
         errorLIst.forEach(e->detalle.put(e.getField(),e.getDefaultMessage()));
         return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(),"Validaciones",detalle);
     }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDTO isExist(ExistException ex){
+        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),null);
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDTO listaIncorrecta(ListaIncorrectaException ex){
+        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),null);
+    }
+    //VER COMO QUEDARIA SI TENGO UN SOLO EXISTEXCEPTION Y USO ESE PARA PELICULA Y GENERO
 }
