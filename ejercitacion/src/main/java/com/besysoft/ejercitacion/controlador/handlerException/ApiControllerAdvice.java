5b2b127b4763931d53738d8375641bb89d20ac89
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-02 17:44:12 -0300

    Create exceptionHandler for those methods that have @Valid

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/handlerException/ApiControllerAdvice.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/handlerException/ApiControllerAdvice.java
@@ -1,8 +1,25 @@
 package com.besysoft.ejercitacion.controlador.handlerException;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
+import com.besysoft.ejercitacion.dto.response.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ApiControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDTO exceptionHandler(MethodArgumentNotValidException ex){
+        List<FieldError> errorLIst= ex.getBindingResult().getFieldErrors();
+        Map<String,String> detalle= new HashMap<>();
+        errorLIst.forEach(e->detalle.put(e.getField(),e.getDefaultMessage()));
+        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(),"Validaciones",detalle);
+    }
 }
