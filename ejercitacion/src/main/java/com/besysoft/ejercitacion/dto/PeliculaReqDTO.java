227a6b2945da49b6d8bbcb1f7a24bb38f3137f9a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-02 20:27:09 -0300

    Update PeliculaReqDTO.java

--- ejercitacion/src/main/java/com/besysoft/ejercitacion/dto/PeliculaReqDTO.java
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/dto/PeliculaReqDTO.java
@@ -18,7 +18,7 @@ public class PeliculaReqDTO {
     private int id;
     @NotNull
     @NotEmpty
-    @Pattern(regexp = "^[A-Za-z]+$", message = "solamente permite caracteres de la A - Z")
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "solamente permite caracteres de la A - Z")
     @Size(min = 3, max = 20)
     private String titulo;
     private LocalDate fechaCreacion;
