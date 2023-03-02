f21bf73d158b86860bbb687178771f5a00946bd9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-02 17:24:32 -0300

    Create ApiControllerAdvice.java

---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/controlador/handlerException/ApiControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.besysoft.ejercitacion.controlador.handlerException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice(annotations = RestController.class)
+public class ApiControllerAdvice {
+}
