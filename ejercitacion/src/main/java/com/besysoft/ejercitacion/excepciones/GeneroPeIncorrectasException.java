d283cd5f5fc33af54a900636ee2eb9c8310fa329
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-02 17:24:27 -0300

    Create GeneroPeIncorrectasException.java

---
+++ ejercitacion/src/main/java/com/besysoft/ejercitacion/excepciones/GeneroPeIncorrectasException.java
@@ -0,0 +1,8 @@
+package com.besysoft.ejercitacion.excepciones;
+
+public class GeneroPeIncorrectasException extends RuntimeException{
+
+    public GeneroPeIncorrectasException(String message) {
+        super(message);
+    }
+}
