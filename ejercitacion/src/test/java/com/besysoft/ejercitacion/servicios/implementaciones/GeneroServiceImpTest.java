e9ed1a22fbc44af69b8a3b2c671f5853c290841c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-25 18:42:42 -0300

    Update GeneroServiceImpTest.java

--- ejercitacion/src/test/java/com/besysoft/ejercitacion/servicios/implementaciones/GeneroServiceImpTest.java
+++ ejercitacion/src/test/java/com/besysoft/ejercitacion/servicios/implementaciones/GeneroServiceImpTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
